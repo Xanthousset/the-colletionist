d06a1b1032a51e23d075f323d40be074077263a9
MathieuB <praisetheelevator@gmail.com>
2026-01-27 17:45:35 +0100

    renamed token env

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import axios from "axios";
 import { createPinia } from 'pinia'
 
 // configuration api authorization sur l'ensemble du projet
-axios.defaults.headers.common['Authorization'] = 'Bearer ' + process.env.SECRET_TOKEN
+axios.defaults.headers.common['Authorization'] = `Bearer ${process.env.VUE_APP_SECRET_TOKEN}`
 
 
 createApp(App).use(router).use(createPinia()).mount('#app')
